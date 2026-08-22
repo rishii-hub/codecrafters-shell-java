023af48ce6bdd5f1fad50eb7241411a4bf941618
rishi <rishii2526@gmail.com>
2026-08-22 22:57:02 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,6 +11,13 @@ public class Main {
                 break;
             } else if (command.startsWith("echo")) {
                 System.out.println(command.substring(5));
+            } else if (command.startsWith("type ")) {
+                if (command.substring(5).equals("echo") || command.substring(5).equals("exit")
+                        || command.substring(5).equals("type")) {
+                    System.out.println(command.substring(5) + " is a shell builtin");
+                } else {
+                    System.out.println(command + ": not found");
+                }
             } else {
                 System.out.println(command + ": command not found");
             }
